60dc8f4cf2e37a5720c5a46197d650c726034d37
David Li <dtli@lcs.on.ca>
2023-10-23 09:13:16 -0400

    Initial Commit

---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by David Li on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
